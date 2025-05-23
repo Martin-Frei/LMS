de09b4ec6c53ecb1209228a65501c928a9163de3
Martin-Frei <mat.frei@gmx.de>
2025-05-23 19:50:34 +0200

    BMS Worked some funktion

--- manageBooks/views.py
+++ manageBooks/views.py
@@ -13,9 +13,14 @@ def removeBook(request):
 
 
 def manageBook(request):
-    context ={}
+    bookname= ['How to built Django','How to biuld Webpages',' How to learn Python','How to learn Java Script']
+    ISBN= [1234567890,1212121212, 3434343434, 5656565656]
+    
+    context ={'bookDetail': zip(bookname, ISBN)}
+    
     return render(request,'manageBookLMS.html', context )
 
 def index(request):
     context ={}
     return render(request,'mainLMS.html', context )
+
